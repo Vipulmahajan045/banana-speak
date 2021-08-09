11004fdc2c4d38deee6d13b8cbb2ec0f20866a03
Vipulmahajan045 <vipulmahajan170140111045@gmail.com>
2021-08-09 11:52:59 +0530

    input output processing done

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
+var divOutput = document.querySelector("#output");
+
+
 function clickHandler(){
-    console.log("clicked!")
-    console.log("input" , txtInput.value)
-}
+    divOutput.innerText = "jdjdjdjdjjdjdjdj " + txtInput.value;
 
-
+};
 
 btnTranslate.addEventListener("click", clickHandler)
   
+//live exercise
+// var element = document.querySelector("textarea");
+// var byClass = document.querySelector(".btn-primary");
+// var byId = document.querySelector("#input-btn");
+// var byAttr = document.querySelector("input[name='translator']")
