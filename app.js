1530674049a5fa93f3e3b3556856e71a30476b46
Vipulmahajan045 <vipulmahajan170140111045@gmail.com>
2021-08-09 20:07:35 +0530

    done

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var divOutput = document.querySelector("#output");
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var serverURL = "https://api.funtranslations.com/translate/pig-latin.json";
+
+function getTranslationURl(text){
+    return serverURL + "?" + "text=" + text
+}
+
+function errorHandler(error){
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time")
+}
 
 
 function clickHandler(){
-    divOutput.innerText = "jdjdjdjdjjdjdjdj " + txtInput.value;
+    var inputText = txtInput.value;    //taking input
+
+    //call the server for processing
+    fetch(getTranslationURl(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translateText = json.contents.translated;
+        divOutput.innerText = translateText;
+    })
+    .catch(errorHandler) 
 
 };
 
